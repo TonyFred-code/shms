d8eec333969486089f388ba0ff1801065cfb4172
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-03-04 13:11:28 +0100

    feat(setup): vite project structure and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./context/authProvider.jsx";
+import DataProvider from "./context/dataProvider.jsx";
+import router from "./routes/route.jsx";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <AuthProvider>
+      <DataProvider>
+        <RouterProvider router={router} />
+      </DataProvider>
+    </AuthProvider>
+  );
 }
